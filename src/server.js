ae30bf9e116413613d4ea78c79c8ad952a573a5f
Gabriel <gabriel.abecher@hotmail.com>
2024-10-18 11:02:29 -0300

    ready to deploy, added cors

--- src/server.js
+++ src/server.js
@@ -1,17 +1,29 @@
 require("dotenv").config();
-const port = process.env.SERVER_PORT;
-const express = require("express");
-const { resolve } = require("path");
 require("./database");
+
+const { resolve } = require("path");
+
+const express = require("express");
+
+const cors = require("cors");
+
 const app = express();
+
 const alunoRoutes = require("./routes/alunoRouter");
 const avisoRoutes = require("./routes/avisoRouter");
 const professorRoutes = require("./routes/professorRouter");
 const turmaRoutes = require("./routes/turmaRouter");
 const fotoRouter = require("./routes/fotoRouter");
+
+const port = process.env.SERVER_PORT;
+
+//Definindo middlewares
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(resolve(__dirname, "..", "uploads")));
+
+//Definindo rotas
 app.use("/alunos", alunoRoutes);
 app.use("/avisos", avisoRoutes);
 app.use("/professores", professorRoutes);
